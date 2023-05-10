5d72a6c52308f7b66adcb002cf4819a3ef38dec1
Oksana Dziuba <dziuba131303@gmail.com>
2023-05-10 22:14:53 +0400

    edits promises task without notiflix

--- src/src/js/03-promises.js
+++ src/src/js/03-promises.js
@@ -1,8 +1,29 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const delayValue = document.querySelector("input[name = 'delay']");
+  const amountValue = document.querySelector("input[name = 'amount']");
+  const stepValue = document.querySelector("input[name = 'step']");
+  for (let i = 1; i <= amountValue.value; i += 1) {
+    createPromise(i, +delayValue.value + stepValue.value * i)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
