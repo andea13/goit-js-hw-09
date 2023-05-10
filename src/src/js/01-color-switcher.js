cb2660f6cde3423138296cc605a51be837fcd28d
Oksana Dziuba <dziuba131303@gmail.com>
2023-05-11 01:30:49 +0400

    finalizes task 1

--- src/src/js/01-color-switcher.js
+++ src/src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  bodyEl: document.querySelector('body'),
+  btnStartEl: document.querySelector('button[data-start]'),
+  btnStopEl: document.querySelector('button[data-stop]'),
+};
 
+const DELAY = 1000;
+let timerId = null;
+
+refs.btnStartEl.addEventListener('click', () => {
+  refs.btnStartEl.disabled = true;
+  refs.btnStopEl.disabled = false;
+
+  timerId = setInterval(event => {
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, DELAY);
+});
+
+refs.btnStopEl.addEventListener('click', () => {
+  clearInterval(timerId);
+  refs.btnStartEl.disabled = false;
+  refs.btnStopEl.disabled = true;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
