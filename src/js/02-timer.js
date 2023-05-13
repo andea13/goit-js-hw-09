f38cdfef557955320f269a5a737ed0dac0f59ff6
Oksana Dziuba <dziuba131303@gmail.com>
2023-05-13 20:18:47 +0400

    finalizes task 2 before final submit

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,23 +4,10 @@
 
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 const now = new Date();
-// let nowInMS = now.getTime();
-// console.log(nowInMS);
-
 let timeDifference = 0;
-let inputTimeValue = 0;
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log((inputTimeValue = selectedDates[0]));
-  },
-};
 
 const refs = {
   fpEl: document.getElementById('datetime-picker'),
@@ -32,6 +19,26 @@ const refs = {
   secondsEl: document.querySelector('[data-seconds]'),
 };
 
+refs.startBtnEl.disabled = true;
+
+let inputTimeValue = 0;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log((inputTimeValue = selectedDates[0].getTime()));
+
+    if (inputTimeValue < now) {
+      Notiflix.Notify.warning('Please choose a date in the future');
+    } else {
+      refs.startBtnEl.disabled = false;
+    }
+  },
+};
+
 // console.log(refs.startBtnEl);
 // console.log(refs.daysEl);
 // console.log(refs.hoursEl);
@@ -40,19 +47,72 @@ const refs = {
 
 const fp = flatpickr(refs.fpEl, options);
 
-refs.fpEl.addEventListener('input', event => {
-  const selectedDates = event.target.value;
-  console.log(selectedDates);
-});
+const handleTime = () => {
+  const now = new Date();
+  const timeDifference = inputTimeValue - now;
+  console.log(timeDifference);
 
-const attachToElement = ({ daysEl, hoursEl, minutesEl, secondsEl }) => {
-  const timeLeftValue = `${daysEl}d ${hoursEl}h ${minutesEl}m ${secondsEl}s`;
-  refs.timerEl.textContent = timeLeftValue;
+  if (timeDifference >= 0) {
+    const { days, hours, minutes, seconds } = convertMs(`${timeDifference}`);
+    console.log(`${days}:${hours}:${minutes}:${seconds}`);
+
+    refs.daysEl.textContent = `${days}`;
+    refs.hoursEl.textContent = `${hours}`;
+    refs.minutesEl.textContent = `${minutes}`;
+    refs.secondsEl.textContent = `${seconds}`;
+  } else {
+    refs.daysEl.textContent = '00';
+    refs.hoursEl.textContent = '00';
+    refs.minutesEl.textContent = '00';
+    refs.secondsEl.textContent = '00';
+  }
 };
 
-attachToElement(refs.timerEl, {
-  daysEl,
-  hoursEl,
-  minutesEl,
-  secondsEl,
-});
+refs.startBtnEl.addEventListener('click', onClickTimerStart);
+
+function onClickTimerStart() {
+  const timer = setInterval(handleTime, 1000);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+const addLeadingZero = value => {
+  return value.toString().padStart(2, '0');
+};
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+// refs.fpEl.addEventListener('input', event => {
+//   const selectedDates = event.target.value;
+//   console.log(selectedDates);
+// });
+
+// const { fpEl, daysEl, hoursEl, minutesEl, secondsEl } = refs;
+
+// const attachToElement = (fpEl, daysEl, hoursEl, minutesEl, secondsEl) => {
+//   const timeLeftValue = `${daysEl}d ${hoursEl}h ${minutesEl}m ${secondsEl}s`;
+//   fpEl.textContent = selectedDates;
+// };
+
+// attachToElement(daysEl, hoursEl, minutesEl, secondsEl);
